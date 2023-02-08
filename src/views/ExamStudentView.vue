1dd11909313f5fa1360319765459e51b9aa1adc7
setiad1 <dev.setiadi.acc@gmail.com>
2023-02-08 16:44:07 +0700

    Update quiz

--- src/views/ExamStudentView.vue
+++ src/views/ExamStudentView.vue
@@ -1,13 +1,14 @@
 <template>
     <div id="main" class="container is-max-desktop p-32 mt-5">
-        <button class="button is-small mb-5">
+        <button class="button is-small is-primary is-outlined mb-5" v-on:click="backTo">
             <span class="icon">
                 <i class="fa-thin fa-note"></i>
             </span>
             <span>Exam List</span>
         </button>
-        <div v-if="show">
-            <h2 class="is-size-5 mb-5 has-text-weight-bold">{{ session_show }}</h2>
+        <div v-if="show" class="mb-5">
+            <h2 class="is-size-5 has-text-weight-bold">{{ session_title }}</h2>
+            <span>{{ session_description }}</span>
         </div>
         <div v-if="!show">
             <div class="tile is-ancestor">
@@ -23,23 +24,49 @@
             </div>
         </div>
         <div v-if="show">
-            <div class="mt-4" v-for="(item, index) in questions" :key="item.question_id">
-                {{ item.ref_question.question }}
-                <div v-for="(t, i) in item.ref_options" :key="t.id">
-                <!-- {{ t.option }} -->
-                <div class="control">
-                    <label class="radio">
-                        <input type="radio" :name="item.question_id"> {{ t.option }}
-                    </label>
-                </div>
+            <div class="columns is-multiline">
+                <div v-for="(item, index) in questions" :key="item.question_id" 
+                    class="column mt-4 is-relative is-half-tablet is-three-quarters-tablet is-full-mobile" style="padding-bottom: 5rem;">
+                    <div class="is-size-6 has-text-weight-bold has-text-grey mb-1">{{ index += 1 }}. {{ item.ref_question.question }}</div>
+                    <div v-for="(t, i) in item.ref_options" :key="t.id">
+                        <div class="control">
+                            <label class="radio">
+                                <input type="radio" v-model="item.selected_answer_id" :name="item.question_id" :value="t.id"> {{ t.option }}
+                            </label>
+                        </div>
+                    </div>
+                    
+                    <div style="position: absolute; bottom: 0; border-bottom: 1px solid #dfdfdf; width: 95%; padding-bottom: 10px;">
+                        <div class="mt-4" v-if="answers">
+                            <span v-for="(i, e) in answers" :key="i">
+                                <span v-if="i.question_id == item.question_id">
+                                    <div v-if="i.s_answer_id == i.r_answer_id" class="control m-0 mb-1 p-0">
+                                        <div class="tags has-addons">
+                                        <span class="tag is-dark"><i class="fa-solid fa-check"></i></span>
+                                        <span class="tag is-success">1</span>
+                                        </div>
+                                    </div>
+                                    <div v-else class="control m-0 mb-1 p-0">
+                                        <div class="tags has-addons">
+                                        <span class="tag is-dark"><i class="fa-solid fa-xmark"></i></span>
+                                        <span class="tag is-danger">0</span>
+                                        </div>
+                                    </div>
+                                    <span>answer: {{ i.r_answer }}</span>
+                                </span>
+                            </span>
+                        </div>
+                        <span v-else>---</span>
+                    </div>
                 </div>
             </div>
-            <div class="mt-4">
-                <button class="button is-small" v-on:click="backTo">
-                    <span class="icon">
+
+            <div class="mt-5">
+                <button class="button is-small is-dark" v-on:click="checkAnswers">
+                    <!-- <span class="icon">
                         <i class="fa-solid fa-arrow-left"></i>
-                    </span>
-                    <span>Back</span>
+                    </span> -->
+                    <span>Check answers</span>
                 </button>
             </div>
         </div>
@@ -54,13 +81,22 @@ import { throwStatement } from '@babel/types';
     data() {
         return {
             show: false,
-            session_show: null,
+            session_id: null,
+            session_title: null,
+            session_description: null,
             sessions: [],
-            questions: []
+            questions: [],
+            selected_answer_id: null,
+            answers: null,
+            counter: 0
         }
     },
     watch: {
-
+    },
+    computed: {
+        selected_answers() {
+            return this.questions.map(item => [item.question_id, item.selected_answer_id])
+        }
     },
     mounted() {
         this.fetchSessions();
@@ -71,7 +107,6 @@ import { throwStatement } from '@babel/types';
                 data: null
             }).then((res) => {
                 this.sessions = res.data;
-                console.log(this.sessions);
             })
         },
         fetchQuestions: async function(v) {
@@ -79,18 +114,58 @@ import { throwStatement } from '@babel/types';
                 session_id: v
             }).then((res) => {
                 if (res.data) {
-                    this.session_show = res.data[0].ref_session.session;
+                    res.data.forEach(e => {
+                        e['selected_answer_id'] = null;
+                    });
+                    this.session_id = res.data[0].ref_session.id;
+                    this.session_title = res.data[0].ref_session.session;
+                    this.session_description = res.data[0].ref_session.session_description;
                     this.questions = res.data;
                     this.show = true;
-                    console.log(this.questions);
                 }
+            })
+        },
+        checkAnswers: async function() {
+            await this.axios.post("http://127.0.0.1:8000/quiz/answers-by-session", {
+                session_id: this.session_id
+            }).then((res) => {
+                var x = [];
+                this.selected_answers.forEach((item, index) => {
+                    x[index] = {};
+                    x[index]['question_id'] = item[0];
+                    x[index]['question'] = res.data[index].ref_question.question;
+                    x[index]['s_answer_id'] = item[1];
+                    if (res.data[index].question_id == item[0]) {
+                        x[index]['r_answer_id'] = res.data[index].ref_answers[0].answer;
+                        res.data[index].ref_options.forEach((i, e) => {
+                            if (i.id == res.data[index].ref_answers[0].answer) {
+                                x[index]['r_answer'] = i.option;
+                            }
+                            if (item[1] == i.id) {
+                                x[index]['s_answer'] = i.option;
+                            }
+                        });
+                    }
+                });
+                this.answers = x;
+                console.log(this.answers);
             })
         },
         backTo: function() {
             this.show = false;
+            this.resetSession();
+            this.fetchSessions();
+        },
+        resetSession: function() {
+            this.show = false;
+            this.session_id = null;
+            this.session_title = null;
+            this.session_description = null;
             this.sessions = [];
             this.questions = [];
-            this.fetchSessions();
+            this.selected_answer_id = null;
+            this.answers = null;
+            this.counter = 0;
         }
     }
   }
